5bfe4ef113acd3fdb265b6abb92d962a9072af57
Julian Kotrba <me@juliankotrba.xyz>
2020-12-04 14:20:32 +0100

    Add template

---
+++ src/main/kotlin/Template.kt
@@ -0,0 +1,15 @@
+// package day0x
+
+import parse
+
+fun main() {
+    val input = parse("src/main/kotlin/day0x/input") {
+
+    }
+
+    println(part1())
+    println(part2())
+}
+
+fun part1(): Unit = TODO()
+fun part2(): Unit = TODO()
