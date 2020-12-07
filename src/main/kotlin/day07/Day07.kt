70a9a8e9f7ec117c3c5d7dce49d345fc6fd98a2b
Julian Kotrba <me@juliankotrba.xyz>
2020-12-07 08:09:11 +0100

    Add day 7 part 2 solution

---
+++ src/main/kotlin/day07/Day07.kt
@@ -0,0 +1,67 @@
+package day07
+
+import parse
+
+
+typealias Bag = String
+typealias Contained = Pair<Bag, Int>
+
+fun main() {
+
+
+    val input = parse("src/main/kotlin/day07/input") { input ->
+        val bagTree = mutableMapOf<Bag, Set<Contained>>()
+        // light red bags contain 1 bright white bag, 2 muted yellow bags
+        input.forEach { s ->
+            val parent = s.split(" bags contain ")[0] // light red
+            val child = s.split(" bags contain ")[1] // 1 bright white bag, 2 muted yellow bags
+
+            val childrenAsList = if (child == "no other bags.") {
+                emptySet()
+            } else {
+                child.dropLast(2).split(", ") // drop "s."
+                        .map {
+                            // 1 bright white bag,
+                            Contained(
+                                    it.split(" ")[1] + " " + it.split(" ")[2],
+                                    it.split(" ")[0].toInt()
+                            )
+                        }.toSet()
+            }
+
+            bagTree[parent] = childrenAsList
+        }
+        bagTree
+    }
+
+    println(part1(input))
+    println(part2(input))
+}
+
+fun part1(bagMap: Map<Bag, Set<Contained>>) =
+        bagMap.map {
+            traversePart1(it.key, bagMap)
+        }.count { it > 0 }
+
+fun traversePart1(currentBag: Bag, bagMap: Map<Bag, Set<Contained>>): Int {
+    // TODO: Abort when first shiny golden bag found
+    //       Currently this solution counts all the occurrences of shiny golden bags
+    if (currentBag == "shiny gold") {
+        return 1
+    }
+    var count = 0
+    bagMap.getValue(currentBag).forEach {
+        count += traversePart1(it.first, bagMap)
+    }
+    return count
+}
+
+fun part2(bagMap: Map<Bag, Set<Contained>>): Int = traversePart2("shiny gold", bagMap)
+
+fun traversePart2(currentBag: Bag, bagMap: Map<Bag, Set<Contained>>): Int {
+    var count = 0
+    bagMap.getValue(currentBag).forEach {
+        count += it.second + it.second*traversePart2(it.first, bagMap)
+    }
+    return count
+}
